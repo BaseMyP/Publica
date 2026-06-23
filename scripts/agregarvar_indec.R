fa10b53009cb62cf9cee9358c80f69ee900f8358
Esteban Rodriguez <esteban.rgz@gmail.com>
2026-06-23 14:19:40 -0300

    Agregar EMAE

--- scripts/agregarvar_indec.R
+++ scripts/agregarvar_indec.R
@@ -1,84 +1,56 @@
 source("scripts/funciones_base.R")
 
-# 1. Definimos los metadatos para el Nivel General
+# 1. Definimos los metadatos 
 meta_ipc_general_corregido <- list(
-  titulo = "IPC Nacional - Nivel General",
-  descripcion = "Índice de Precios al Consumidor, cobertura nacional. Nivel General. Base Diciembre 2016=100.",
+  titulo = "EMAE Sin Estacionalidad. Índice Base 2004.Valores mensuales",
+  descripcion = "Estimador Mensual de Actividad Económica (EMAE). Base 2004. Desestacionalizado",
   pais = "Argentina",
-  categoria = "PRECIOS",
+  categoria = "ACTIVIDAD",
   frecuencia_short = "M",
   frecuencia_original = "mensual",
   unidades = "Índice", 
-  ajuste = "NSA",
+  ajuste = "SA",
   tipo_informacion = "Pública",
   fuente = "API_Argentina",
   fuente_original = "INDEC",
-  fuente_formato = "API_Argentina",
-  id_original = "148.3_INIVELNAL_DICI_M_26",
+  fuente_formato = "API_INDEC",
+  id_original = "143.3_NO_PR_2004_A_31",
   ultima_actualizacion = Sys.Date(),
-  fecha_inicio = as.Date("2003-01-01"),
-  url_original = "https://www.indec.gob.ar/indec/web/Nivel4-Tema-3-5-31",
-  revisable = FALSE,
+  fecha_inicio = as.Date("2004-01-01"),
+  url_original = "https://datosgobar.github.io/series-tiempo-ar-explorer/#/series/?ids=143.3_NO_PR_2004_A_31",
+  revisable = TRUE,
   notas = NA
 )
 
-serie_id_general <- "IPCGENERAL_INDICE_NSA_M"
-
-# 2. Definimos los metadatos para el IPC Núcleo
-meta_ipc_nucleo <- list(
-  titulo = "IPC Nacional - Núcleo",
-  descripcion = "Índice de Precios al Consumidor, cobertura nacional. IPC Núcleo. Base Diciembre 2016=100.",
-  pais = "Argentina",
-  categoria = "PRECIOS",
-  frecuencia_short = "M",
-  frecuencia_original = "mensual",
-  unidades = "Índice", 
-  ajuste = "NSA",
-  tipo_informacion = "Pública",
-  fuente = "API_Argentina",
-  fuente_original = "INDEC",
-  fuente_formato = "API_Argentina",
-  id_original = "148.3_INUCLEONAL_DICI_M_19",
-  ultima_actualizacion = Sys.Date(),
-  fecha_inicio = as.Date("2003-01-01"),
-  url_original = "https://www.indec.gob.ar/indec/web/Nivel4-Tema-3-5-31",
-  revisable = FALSE,
-  notas = NA 
-)
-
-serie_id_nucleo <- "IPCNUCLEO_INDICE_NSA_M"
+serie_id_general <- "EMAE_INDICE_SA_M"
 
 # 3. Consolidamos en una lista para procesar en bucle
 # (Se corrigió la llamada a 'meta_ipc_general_corregido')
 lista_series <- list(
-  list(meta = meta_ipc_general_corregido, id = serie_id_general),
-  list(meta = meta_ipc_nucleo, id = serie_id_nucleo)
+  list(meta = meta_ipc_general_corregido, id = serie_id_general)
 )
 
 message("Iniciando carga inicial masiva desde INDEC...")
 
 # Definimos el tema explícitamente para todo este bloque de series
-tema_fijo <- "PRECIOS"
+tema_fijo <- meta_ipc_general_corregido$categoria
 
 # 4. Bucle de extracción, creación y catalogación
-for (item in lista_series) {
-  
-  exito <- update_indec_json_serie(
-    id_indec = item$meta$id_original,
-    serie_id = item$id,
-    tema = tema_fijo,
-    metadatos_fijos = item$meta
+exito <- update_indec_json_serie(
+  id_indec = meta_ipc_general_corregido$id_original,
+  serie_id = serie_id_general,
+  tema = tema_fijo,
+  metadatos_fijos = meta_ipc_general_corregido
+)
+
+if (exito) {
+  # Agregamos el parámetro "tema" explícitamente a update_catalogo
+  update_catalogo(
+    serie_id = serie_id_general, 
+    metadatos = meta_ipc_general_corregido, 
+    metodo_etl = meta_ipc_general_corregido$fuente_formato,
+    tema = tema_fijo
   )
-  
-  if (exito) {
-    # Agregamos el parámetro "tema" explícitamente a update_catalogo
-    update_catalogo(
-      serie_id = item$id, 
-      metadatos = item$meta, 
-      metodo_etl = "API_INDEC",
-      tema = tema_fijo
-    )
-  }
 }
 
 message("¡Carga finalizada! Revisa tu carpeta PRECIOS/ y tu catalogo.json.")
