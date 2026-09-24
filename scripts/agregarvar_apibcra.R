db24f81e6fd7e0ada64a3b90f5f1c8b0945ce190
Esteban Rodriguez <esteban.rgz@gmail.com>
2026-09-24 11:13:08 -0300

    series de préstamos en USD

--- scripts/agregarvar_apibcra.R
+++ scripts/agregarvar_apibcra.R
@@ -7,30 +7,30 @@ source("scripts/funciones_base.R")
       
 # 1. Definimos los metadatos fijos para la nueva variable
 meta_reservas <- list(
-  titulo = "Tipo de Cambio Pesos/DEG",
-  descripcion = "Tipo de Cambio Pesos/DEG",
+  titulo = "Préstamos en USD al sector privado. Otros",
+  descripcion = "Saldo de otros préstamos en dólares otorgados al sector privado (en millones de USD)",
   pais = "Argentina",
-  categoria = "TC_y_Tasas",
+  categoria = "PRESTAMOS",
   frecuencia_short = "D",
   frecuencia_original = "diaria",
-  unidades = "Pesos/DEG", 
+  unidades = "Millones de USD", 
   ajuste = "NSA",
   tipo_informacion = "Pública",
   fuente = "BCRA",
   fuente_original = "BCRA",
-  #fuente_formato = "API_BCRA",
-  fuente_formato = "API_BCRA_Cambiarias",
-  id_original = "XDR",
+  fuente_formato = "API_BCRA",
+  #fuente_formato = "API_BCRA_Cambiarias",
+  id_original = 124,
   ultima_actualizacion = Sys.Date(),
-  fecha_inicio = as.Date("2018-05-14"),
-  url_original = "https://www.bcra.gob.ar/evolucion-moneda/",
+  fecha_inicio = as.Date("2002-12-31"),
+  url_original = "https://www.bcra.gob.ar/archivos/Pdfs/PublicacionesEstadisticas/series.xlsm",
   revisable = FALSE,
   notas = NA 
 )
 
 
 # 2. Definimos el nombre oficial de la serie
-serie_id_reservas <- "DEG_PESOSporDEG_NSA_D"
+serie_id_reservas <- "enUSD_SECTORPRIV_OTROS_NSA_D"
  
 BASE_URL_MONETARIAS <- "https://api.bcra.gob.ar/estadisticas/v4.0/Monetarias/"
 BASE_URL_CAMBIARIAS <- "https://api.bcra.gob.ar/estadisticascambiarias/v1.0/Cotizaciones/"
@@ -49,8 +49,8 @@ if (exito_reservas) {
   update_catalogo(
     serie_id = serie_id_reservas, 
     metadatos = meta_reservas,
-    #metodo_etl = "API_BCRA",
-    metodo_etl = "API_BCRA_Cambiarias",
+    metodo_etl = "API_BCRA",
+    #metodo_etl = "API_BCRA_Cambiarias",
     tema = meta_reservas$categoria         # <--- CAMBIO: Agregamos la nueva carpeta explícita
   )
   message("¡Serie de Reservas Internacionales agregada con éxito a la base de datos!")
